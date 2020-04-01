03299b051a2322f4259aa597b75acc5da835feda
noteasycode <58310014+noteasycode@users.noreply.github.com>
2020-04-01 10:57:35 +0300

    task4

--- classes.py
+++ classes.py
@@ -1,16 +1,16 @@
+import math
 from functools import reduce
 
 
 class First:
-    def __init__(self, number, divisor):
+    def __init__(self, number):
         self.number = number
-        self.divisor = divisor
 
-    def is_divisible_by(self):
-        if (self.number % self.divisor) == 0:
-            print(f"The number {self.number} is divisible by number {self.divisor} without remainder.")
+    def is_divisible_by(self, divisor):
+        if (int(self.number) % divisor) == 0:
+            print(f"The number {self.number} is divisible by number {divisor} without remainder.")
         else:
-            print(f"The number {self.number} is not divisible by number {self.divisor} without remainder.")
+            print(f"The number {self.number} is not divisible by number {divisor} without remainder.")
 
     @staticmethod
     def stat_method(num1, num2):
@@ -18,29 +18,44 @@ class First:
         print(f"The result of multiply is {multiply(num1, num2)}")
 
     def __del__(self):
-        print(f"The number {self.number}, was delete")
-        print(f"The number {self.divisor}, was delete")
-
-
-div_number = First(10, 3)
-div_number.is_divisible_by()
-First.stat_method(2, 3)
+        print(f"The object {self.__class__}, was delete")
 
 
 class Second(First):
-    def __init__(self, number, divisor, color, string):
-        super().__init__(number, divisor)
-        self.string = string
+    def __init__(self, number, color):
+        super().__init__(number)
+        self.obj = number
         self.__color = color
 
     def __checker(self):
-        return type(str()) == type(self.string)
+        return type(self.obj) == str
+
+    def str_checker(self):
+        return "is a string" if self.__checker() else "is not a string"
 
 
-print("Second class:")
-second_var = Second(10, 15, 5, 6)
-print(f"The protected variable is: {second_var._Second__color}")
+class Third(First):
 
+    @staticmethod
+    def stat_method(num1, num2):
+        print(f"The Greatest Common Divisor of {num1} and {num2} is {math.gcd(num1, num2)}")
+        print(f"The lowest common multiple of {num1} and {num2} is {(num1 * num2) // math.gcd(num1, num2)}")
+
+
+
+if __name__ == "__main__":
+    print("The result of operations in Third class:")
+    first_var = First(10)
+    first_var.is_divisible_by(3)
+    First.stat_method(2, 3)
+    print("The result of operations in Second class:")
+    second_var = Second('20', 15)
+    print(f"The protected variable is: {second_var._Second__color}")
+    print(f"The variable {second_var.number} {second_var.str_checker()}")
+    second_var.is_divisible_by(10)
+    print("The result of operations in Third class:")
+    third_var = Third(11)
+    Third.stat_method(85, 45)
 
 """
 # Use map to print the square of each numbers rounded
