0f6e9c53b740b89ca2dfdeaea35405b28f93ce33
noteasycode <58310014+noteasycode@users.noreply.github.com>
2020-04-29 17:13:17 +0300

    task4

--- mytestsite/locations/urls.py
+++ mytestsite/locations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from locations import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('index_with_arg/<str:add_something>', views.index_with_arg),
 ]
--- mytestsite/locations/views.py
+++ mytestsite/locations/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the locations index.")
+
+
+def index_with_arg(request, add_something):
+    return render(request, 'test.html', {'press': add_something})
