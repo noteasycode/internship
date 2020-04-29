e46e55ab2b0232001f1eb07f277e0718bc2a328c
noteasycode <denis.kr.pr@gmail.com>
2020-04-29 20:45:47 +0300

    task7

--- mytestsite/locations/views.py
+++ mytestsite/locations/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the locations index.")
+
+
+def display_route(request, route):
+    """Displays entered route"""
+    text = '<h3>Displaying entered route: {}</h3>'.format(route)
+    back = '<p><a style="color:blue" href="http://127.0.0.1:8000/"><< Back</a></p>'
+    response = text + back
+    return HttpResponse(response)
